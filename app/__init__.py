518792c703a65d785ec41207186eb27d5bb9ff9c
manuel borowski <emmanuel.borowski@gmail.com>
2021-03-02 08:32:37 +0100

    V1.0 : copy from student-booklet

---
+++ app/__init__.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, abort
+import logging.handlers, os, sys
+
+app = Flask(__name__, instance_relative_config=True)
+
+# V1.0 : copy from student-booklet
+
+app.config['version'] = 'V1.0'
+
+# enable logging
+LOG_HANDLE = 'soauth'
+log = logging.getLogger(LOG_HANDLE)
+
+# local imports
+from config import app_config
+
+
+# support custom filtering while logging
+class MyLogFilter(logging.Filter):
+    def filter(self, record):
+        record.username = 'NONE'
+        return True
+
+config_name = os.getenv('FLASK_CONFIG')
+config_name = config_name if config_name else 'production'
+
+# set up logging
+LOG_FILENAME = os.path.join(sys.path[0], app_config[config_name].STATIC_PATH, 'log/soauth-log.txt')
+try:
+    log_level = getattr(logging, app_config[config_name].LOG_LEVEL)
+except:
+    log_level = getattr(logging, 'INFO')
+log.setLevel(log_level)
+log.addFilter(MyLogFilter())
+log_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10 * 1024, backupCount=5)
+log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(username)s - %(message)s')
+log_handler.setFormatter(log_formatter)
+log.addHandler(log_handler)
+
+log.info('start SOAUTH')
+
+app.config.from_object(app_config[config_name])
+app.config.from_pyfile('config.py')
+
+from app.view.auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
---
+++ app/view/auth/views.py
@@ -0,0 +1,40 @@
+from flask import redirect, render_template, url_for, request, session
+from app import log, app
+from . import auth
+import json
+from authlib.flask.client import OAuth
+
+oauth = OAuth(app)
+smartschool = oauth.register('smartschool')
+
+
+@auth.route('/', methods=['GET', 'POST'])
+def login():
+    if 'app_uri' in request.args:
+        session['app_uri'] = request.args['app_uri']
+        # Step 1 : go to smartschool so that the user can log in with smartschool credentials
+        redirect_uri = url_for('auth.login', _external=True, _scheme='https')
+        return oauth.smartschool.authorize_redirect(redirect_uri)
+
+    if 'code' in request.args:  # received a request with a OAUTH code
+        # Step 2 : with the code from smartschool, fetch the access_token from smartschool
+        token = oauth.smartschool.authorize_access_token()
+        return redirect(url_for('auth.smartschool_profile', token=json.dumps(token)))
+
+# OAUTH specific
+@auth.route('/smartschool_profile/<string:token>', methods=['GET', 'POST'])
+def smartschool_profile(token):
+    # Step 3 : with the access_code, get the userinfo from SS
+    resp = oauth.smartschool.get('fulluserinfo', token=json.loads(token))
+    profile = resp.json()
+
+    if 'app_uri' in session:
+        app_uri = session['app_uri']
+        profile = json.dumps(profile)
+        version = app.config['version']
+        uri = f'{app_uri}?profile={profile}&version={version}'
+        log.info(f'retrieved profile for {uri}')
+        return redirect(uri)
+
+    log.error('no app_uri found in session')
+
