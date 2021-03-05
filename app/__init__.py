a82e8bb6305a77c0a762fb4452e1d28be1063c1e
manuel borowski <emmanuel.borowski@gmail.com>
2021-03-05 12:12:08 +0100

    V1.4 : small bugfix

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,9 @@ app = Flask(__name__, instance_relative_config=True)
 # V1.1 : bugfix service-start-file
 # V1.2 : small bugfixes
 # V1.3 : bugfix redirect_uri
+# V1.4 : small bugfix
 
-app.config['version'] = 'V1.3'
+app.config['version'] = 'V1.4'
 
 # enable logging
 LOG_HANDLE = 'soauth'
